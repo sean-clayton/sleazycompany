3465ab20416307b0cd015f89e8202b38f117d19b
skulbuny <sean@skulbuny.com>
2014-03-10 20:17:17 -0400

    MOOOARRR CHANGE

--- models/posts.js
+++ models/posts.js
@@ -8,11 +8,11 @@ var Post = new keystone.List('Post', {
 
 Post.add({
 	title: { type: String, required: true },
+	subtitle: { type: String },
 	slug: { type: String, index: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true },
-	image: { type: Types.CloudinaryImage },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 }
