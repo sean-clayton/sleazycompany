c1bd906540f2c89914666cd9127cff171de210ac
skulbuny <sean@skulbuny.com>
2014-04-01 18:34:38 -0400

    updated hook

--- hook.js
+++ hook.js
@@ -3,12 +3,12 @@ var gith = require('gith').create( 9001 );
 var execFile = require('child_process').execFile;
 
 gith({
-  repo: 'fideloper/example'
+  repo: 'skulbuny/sleazycompany'
 }).on( 'all', function( payload ) {
   if( payload.branch === 'master' )
   {
     // Exec a shell script
-    execFile('/path/to/hook.sh', function(error, stdout, stderr) {
+    execFile('../sleazycompany.hook.sh', function(error, stdout, stderr) {
       // Log success in some manner
       console.log( 'exec complete' );
     });
